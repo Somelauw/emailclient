42456ef49fd0177d04ee121ba00c59f2a2edacbd
Somelauw <trilili>
2012-07-13 14:19:19 +0200

    EmailProtocols updated

--- EmailProtocol.py
+++ EmailProtocol.py
@@ -1,10 +1,12 @@
-import config
+#import config
 import imaplib
 import smtplib
 
-def getAccounts(fileName):
-    accounts = config.load([fileName])
-    return [EmailAccount(account) for account in accounts]
+class AccountHandler(object):
+    def __init__(self, accounts=[]):
+        #accountConfig = config.Config("emails")
+        #accounts = accountConfig.config
+        self.accounts = [EmailAccount(account) for account in accounts]
 
 class EmailAccount(object):
     def __init__(self, data):
@@ -18,59 +20,25 @@ class EmailAccount(object):
 
         # Simplify the life of users of common protocols
         if self.adress.endswith("@gmail.com"):
-            self.imap = self.imap or Imap("imap.gmail.com", 587)
-            self.smtp = self.smtp or Smtp("smtp.gmail.com", 587)
+            self.receiver = self.receiver or IMAP("imap.gmail.com:587")
+            self.sender = self.sender or SMTP("smtp.gmail.com:587")
 
-        self.sender.login()
-        self.receiver.login()
-
-    def __setitem__(self, key, item):
+    def __setitem__(self, key, value):
         if key == "email":
             self.email = value
         elif key == "password":
             self.password = value
         elif key == "smtp":
-            smtp = value.split(":")
-            if len(smtp) == 1:
-                self.sender = Smtp(smtp, 25)
-            else:
-                self.sender = Smtp(smtp[0], smtp[1])
+            self.receiver = SMTP(self, value)
         elif key == "imap":
-            imap = value.split(":")
-            if len(imap) == 1:
-                self.receiver = Imap(imap, 143)
-            else:
-                self.receiver = Imap(imap[0], imap[1])
+            self.sender = IMAP(self, value)
         elif key == "pop":
             raise Exception("pop is currently not supported")
         else:
             raise Warning("key %s is ignored" % key)
 
-    def getlabels():
-        return self.imap
-
-class Smtp(Sender):
-    def __init__(server, port):
-        self.server = server
-        self.port = port
-
-class Imap(Receiver):
-    def __init__(server, port):
-        self.server = server
-        self.port = port
-
-    def login(self):
-        m = imaplib.IMAP4(self.server, self.port)
-        self.mail = imaplib.IMAP4_SSL(self.server)
-        return self.mail.login(self.email, self.password)
-
     def getlabels(self):
-        return self.mail.list()
-
-# I don't plan on supporting Pop3 anytime soon, so you would have to program this yourself
-#class Pop3(object):
-    #port = 110
-    #pass
+        return self.imap
 
 class Sender(object):
     def login():
@@ -79,4 +47,45 @@ class Sender(object):
 class Receiver(object):
     def login():
         pass
+
+class SMTP(Sender):
+    def __init__(self, account, data):
+        self.account = account
+        server, port = parseServer(data)
+        self.server = smtplib.SMTP(server, port)
+
+    def sendmail(self, to, message):
+        server = self.server
+        server.starttls()
+        server.ehlo()
+        server.login(self.account.email, self.account.password)
+        server.sendmail(self.account.email, to, message)
+        return server.ehlo()
+
+class IMAP(Receiver):
+    def __init__(self, account, data):
+        self.account = account
+        self.server, self.port = parseServer(data)
+        print self.login()
+
+    def login(self):
+        imaplib.IMAP4(self.server, self.port)
+        self.mail = imaplib.IMAP4_SSL(self.server)
+        return self.mail.login(self.account.email, self.account.password)
+
+    def getlabels(self):
+        return self.mail.list()
+
+def parseServer(serverstr):
+    server = serverstr.split(":")
+    if len(server) == 1:
+        return (server, 25)
+    else:
+        return (server[0], int(server[1]))
+
+# I don't plan on supporting Pop3 anytime soon, so you would have to program this yourself
+#class Pop3(object):
+    #port = 110
+    #pass
+
 
