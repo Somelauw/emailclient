2f9039359ae8198f13014603a49b9fe0185492ad
Somelauw <trilili>
2012-07-28 12:48:48 +0200

    ui modified

--- ui.py
+++ ui.py
@@ -4,7 +4,11 @@ import configs
 class App: 
     def __init__(self):
         emails = [email["email"] for email in configs.emails.config]
-        self.menu = Menu("", emails)
+        self.menu = Menu("Emailadress:", emails)
+        cm = urwid.command_map#.copy()
+        cm["j"] = "cursor down"
+        cm["k"] = "cursor up"
+        #self._command_map = cm
 
     def start(self):
         palette = [('reversed', 'standout', '')]
@@ -12,7 +16,13 @@ class App:
         loop.run()
 
     def update(self, input):
+        if input in ("q", "Q"):
+            #print "exit"
+            urwid.ExitMainLoop()
         self.menu.update(input)
+
+        #cm = CommandMap(); cm['j'] = 'cursor up'; cm['k'] = 'cursor down'; my_listbox._command_map = cm
+         #cm = my_listbox._command_map.copy()
 
 class Selectable(urwid.WidgetWrap):
     def __init__(self, *param, **kw):
@@ -36,12 +46,52 @@ class Menu(urwid.WidgetWrap):
                 [urwid.Text(caption)] +\
                 [Selectable(item) for item in self.items])
 
-        self.listbox = urwid.ListBox(self.content)
-        urwid.WidgetWrap.__init__(self, self.listbox)
+        self.listbox = listbox = urwid.ListBox(self.content)
+        urwid.WidgetWrap.__init__(self, listbox)
 
-        # TODO add title
+    #def next(self, shift=1):
+        #(frame, index) = self.content.get_focus()
+        #(frame, index) = self.content.get_next(index)
+        #print index
+        #self.content.set_focus(index)
+
+    #def prev(self, shift=1):
+        #(frame, index) = self.content.get_focus()
+        #(frame, index) = self.content.get_prev(index)
+        #print index
+        #self.content.set_focus(index)
+
+    #def move_selection(self, movement):
+        #(frame, index) = self.content.get_focus()
+        #index += movement
+        #index %= len(self.content)
+        #self.content.set_focus(index)
+
+    def up(self, size):
+        return self.listbox.keypress(size, "up")
+
+    def down(self, size):
+        return self.listbox.keypress(size, "down")
+
+    #def keypress(self, size, key):
+        #self.listbox._command_map["j"] = "cursor down"
+        #self.listbox._command_map["k"] = "cursor up"
+
+        #mappings = {
+                ##"j": self.down,
+                ##"k": self.up,
+                #}
+
+        ## Fall back on default behaviour
+        #return mappings.get(key, lambda size: self.listbox.keypress(size, key))(size)
 
     def update(self, input):
+        print input
+        if input in ("q", "Q"):
+            #print "exit"
+            urwid.ExitMainLoop()
+            # FIXME Doesn't exit
+
         return True
 
 App().start()
